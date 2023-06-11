93a44477d87613e9c046988e01c8c91c4cfd62bc
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-06-11 16:19:39 -0500

    component Card & implementation fakestore API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react"
 import Card from "../../Components/Card"
 
 function Home() {
+
+  const [products,setProducts]=useState(null);
+
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response=> response.json())
+    .then(data=> setProducts(data))
+
+  }, [])
 
   return (
     <>
       <div className='App bg-cyan-100 text-center'>
         Home
-        <Card/>
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {
+          products?.map(product=>(
+            <Card key={product.id} 
+            data={product}/>
+          ))
+        } 
+        </div>
+     
+       
       </div>
      
     </>
