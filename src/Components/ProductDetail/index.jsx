dda7b10d0aa7c59c5c2470fab98d65969c400171
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-06-15 14:32:11 -0500

    show products in ProductDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import { useContext } from 'react'
+import { PerfumesContext } from '../../Context'
 
 const ProductDetail = () => {
+
+  const {isProductDetailOpen,closeProductDetail,productToShow}=useContext(PerfumesContext);
+
   return (
-    <aside className='flex flex-col fixed right-[20px] border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]'>
+    <aside className={`${isProductDetailOpen? 'flex':'hidden'} flex-col fixed right-[20px] border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)] `}>
    
    <div className='flex justify-between items-center p-6'>
     <h2 className='font-medium text-xl'>Detail</h2>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+    <svg onClick={()=>closeProductDetail()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 cursor-pointer">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 
 
 
    </div>
+
+   <figure className='px-6'>
+    <img 
+    className='w-full h-full rounded-lg' 
+    src={productToShow.images[0]} 
+    alt={productToShow.title} />
+   </figure>
+   <p className='flex flex-col p-6'>
+    <span className='font-medium text-2xl mb-2'>${productToShow.price}</span>
+    <span className='font-medium text-md'>${productToShow.title}</span>
+    <span className='font-light text-sm'>${productToShow.description}</span>
+
+   </p>
 
 
 
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,8 +3,19 @@ import { createContext,useState } from "react";
 export const PerfumesContext = createContext();
 
 export const PerfumesProvider = ({ children }) => {
-
+////Shoping Cart-increment quantity
     const [count,setCount]=useState(0);
+
+      //Product Detail -Open/Close
+    const [isProductDetailOpen,setisProductDetailOpen]= useState(false);
+    const openProductDetail =()=> setisProductDetailOpen(true);
+    const closeProductDetail =()=> setisProductDetailOpen(false);
+
+
+    //Product Detail -Show Product
+const [productToShow, setProductToShow]=useState([]);
+
+    
 
     console.log(count);
 
@@ -13,6 +24,12 @@ export const PerfumesProvider = ({ children }) => {
         <PerfumesContext.Provider value= {{
             count,
             setCount,
+            isProductDetailOpen,
+            setisProductDetailOpen,
+            openProductDetail,
+            closeProductDetail,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </PerfumesContext.Provider>
