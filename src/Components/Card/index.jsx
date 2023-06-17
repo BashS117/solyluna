cf3393254edc3c0eb97e5f519cc1d0666d10569b
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-06-17 12:55:03 -0500

    Avoiding duplicate products in the cart & scroll-shoppingCart

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -31,7 +31,35 @@ const Card = (data) => {
         
     }
     
-    
+    const renderIcon = (id) => {
+        const isInCart = cartProducts.filter(product => product.id === id).length > 0;
+        if (isInCart) {
+            return (<div
+                className=" bg-green-400  rounded-full m-2 p-1 absolute top-0 right-0 flex justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                </svg>
+            </div>
+
+            )
+
+        } else {
+            return (
+
+                <div
+                    className=" bg-white  rounded-full m-2 p-1 absolute top-0 right-0 flex justify-center items-center">
+                    <svg
+                        onClick={(event) => addProductsToCart(event, data.data)}
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+
+            )
+        }
+
+    }
+
 
     return (
         <div
@@ -42,15 +70,9 @@ const Card = (data) => {
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 p-2'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
 
-                <div
-                 className=" bg-white  rounded-full m-2 p-1 absolute top-0 right-0 flex justify-center items-center">
-                    <svg
-                    onClick={(event)=>addProductsToCart(event,data.data)}
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </div>
 
+
+                {renderIcon(data.data.id)}
 
 
             </figure>
